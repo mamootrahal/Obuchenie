8de9af3b5bb12e072e729bc88e8aea2e2da6039a
mamootrahal <carev.1910@mail.ru>
2024-03-28 10:00:48 +0300

    лол сипп

---
+++ CPP/C1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int a = 2e2 ;
+
+int main()
+{
+    int b = 22;
+    cout << "Hello World!\n" << endl;
+    printf("Hello, World!\n%d", a);
+    getchar();
+
+    return 0;
+}
